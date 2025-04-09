329e086984bdadb4e3fd474f70e65f2fcc76503f
XIA0LING CUI <10430@holbertonstudents.com>
2025-04-09 16:11:23 +1000

    Update 2-selection_sort.c
    sorted part (0 to i-1)
    unsorted part (i to size -1)

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -5,33 +5,33 @@
  * selection_sort - sorts an array of integers in ascending order
  * @array: pointer to the given array
  * @size: number of elements in the given array
- * Description: - outer loop to build the sorted part (from 0 to i-1), and then in each iteration to 
- *                           find the current smallest value in the unsorted part(from i to size -2)
+ * Description: - outer loop for each iteration, select the current smallest value in the unsorted part(from i to size -2)
+                             and place it at the end of sorted part (from 0 to i-1)
  *              - inner loop to compare the unsorted part(i+1 to size-1) to see if there is a smaller one
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, i_min, temp;
+	size_t i, j, min_index, temp;
 
 	if (array == NULL || size < 2)
 		return;
 
 	for (i = 0; i < size - 1; i++)   /* outer: to build the sorted part (from 0 to i-1) */
 	{
-		i_min = i;               /* set the index of current min (i_min) to i initially */
+		min_index = i;               /* sets min_index (the index of the current minimum) to i initially */
 
 		for (j = i + 1; j < size; j++) /* inner: compare the rest array(i+1 to size-1) to see if there is a smaller one */
 		{
-			if (array[j] < array[i_min])
+			if (array[j] < array[min_index])
 			{
-				i_min = j;       /* found a new min, update i_min */
+				min_index = j;       /* found a new min, update min_index */
 			}
 		}
-		if (i != i_min)
+		if (i != min_index)
 		{
 			temp = array[i];
-			array[i] = array[i_min];
-			array[i_min] = temp;
+			array[i] = array[min_index];
+			array[min_index] = temp;
 			print_array(array, size);
 		}
 	}
