e6c6f92e0b6327dc5bebdfbc9f88582bb73de017
XIA0LING CUI <10430@holbertonstudents.com>
2025-04-09 17:02:12 +1000

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,6 +6,7 @@
  *                the Bubble sort algorithm
  * @array: pointer to the given array
  * @size: number of elements in the given array
+ * Description: - outer loop, for each iteration build the sorted part of array by adding on more 
  */
 void bubble_sort(int *array, size_t size)
 {
@@ -14,11 +15,11 @@ void bubble_sort(int *array, size_t size)
 	if (array == NULL || size < 2)
 		return;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size - 1; i++)                      /* outer loop to control */
 	{
-		flag = 0;
+		flag = 0;                                   /* set flag to 0, means no swap occurs yet */
 
-		for (j = 0; j < size - 1 - i; j++)
+		for (j = 0; j < size - 1 - i; j++)          /* */
 		{
 			if (array[j] > array[j + 1])
 			{
