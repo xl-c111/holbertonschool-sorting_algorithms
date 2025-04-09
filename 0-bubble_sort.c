49cb571c9051e14c7a14dcca21503c40e4544d8b
xl-c111 <10430@holbertonstudents.com>
2025-04-09 06:47:41 +0000

    fix mixes declarations and statements

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,10 +9,10 @@
  */
 void bubble_sort(int *array, size_t size)
 {
+	size_t i, j, flag, temp;
+
 	if (array == NULL || size < 2)
 		return;
-
-	size_t i, j, flag, temp;
 
 	for (i = 0; i < size - 1; i++)
 	{
