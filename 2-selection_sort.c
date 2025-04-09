b6f3230f52c7e130cbc845f3b6fe75a54fb12b30
XIA0LING CUI <10430@holbertonstudents.com>
2025-04-09 15:46:53 +1000

    Update 2-selection_sort.c
    sorted part (from 0 to i-1)
    unsorted part (from i+1 to size -1)

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -5,7 +5,9 @@
  * selection_sort - sorts an array of integers in ascending order
  * @array: pointer to the given array
  * @size: number of elements in the given array
- *
+ * Description: - outer loop to build the sorted part (from 0 to i-1), and then in each iteration to 
+ *                           find the current smallest value in the unsorted part(from i to size -2)
+ *              - inner loop to compare the unsorted part(i+1 to size-1) to see if there is a smaller one
  */
 void selection_sort(int *array, size_t size)
 {
@@ -14,15 +16,15 @@ void selection_sort(int *array, size_t size)
 	if (array == NULL || size < 2)
 		return;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size - 1; i++)   /* outer: to build the sorted part (from 0 to i-1) */
 	{
-		i_min = i;
+		i_min = i;               /* set the index of current min (i_min) to i initially */
 
-		for (j = i + 1; j < size; j++)
+		for (j = i + 1; j < size; j++) /* inner: compare the rest array(i+1 to size-1) to see if there is a smaller one */
 		{
 			if (array[j] < array[i_min])
 			{
-				i_min = j;
+				i_min = j;       /* found a new min, update i_min */
 			}
 		}
 		if (i != i_min)
