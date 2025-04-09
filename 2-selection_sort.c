0377df5bced22ff0c4997c460761a0611edb8249
XIA0LING CUI <10430@holbertonstudents.com>
2025-04-09 16:12:13 +1000

    Update 2-selection_sort.c
    [0, i-1]（Sorted）
    [i, size-1]（Unsorted）

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -6,7 +6,7 @@
  * @array: pointer to the given array
  * @size: number of elements in the given array
  * Description: - outer loop for each iteration, select the current smallest value in the unsorted part(from i to size -2)
-                             and place it at the end of sorted part (from 0 to i-1)
+ *                           and place it at the end of the sorted part (from 0 to i-1)
  *              - inner loop to compare the unsorted part(i+1 to size-1) to see if there is a smaller one
  */
 void selection_sort(int *array, size_t size)
