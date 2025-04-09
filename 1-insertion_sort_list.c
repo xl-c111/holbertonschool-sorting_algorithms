38d52d477a96f3a38c7ae45ac99d0fb290ae7972
xl-c111 <10430@holbertonstudents.com>
2025-04-09 10:40:11 +0000

    fix output error

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,7 +10,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *current, *prev, *next;
 
-	if (list == NULL || *list == NULL || (*list)->next != NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
 	current = (*list)->next;
