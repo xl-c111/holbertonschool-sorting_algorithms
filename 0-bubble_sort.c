5e41c0392e91b9450fb05375ddf2f0cde5485ba9
XIA0LING CUI <10430@holbertonstudents.com>
2025-04-09 18:52:56 +1000

    Update 0-bubble_sort.c
    sorted part (0 to size - i -1)
    unsorted part (size -i to size -1)

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,7 +6,8 @@
  *                the Bubble sort algorithm
  * @array: pointer to the given array
  * @size: number of elements in the given array
- * Description: - outer loop, for each iteration build the sorted part of array by adding on more 
+ * Description: - outer loop, for each iteration build the sorted part of the array with positions from size-i to size-1
+ *              - inner loop, compares adjacent elements, place the largest unsorted element at the end(0 to size-i-1)
  */
 void bubble_sort(int *array, size_t size)
 {
@@ -15,18 +16,18 @@ void bubble_sort(int *array, size_t size)
 	if (array == NULL || size < 2)
 		return;
 
-	for (i = 0; i < size - 1; i++)                      /* outer loop to control */
+	for (i = 0; i < size - 1; i++)                  /* outer loop to build sorted part(size-i to size-1) */
 	{
-		flag = 0;                                   /* set flag to 0, means no swap occurs yet */
+		flag = 0;                               /* set flag to 0, means no swap occurs yet */
 
-		for (j = 0; j < size - 1 - i; j++)          /* */
+		for (j = 0; j < size - 1 - i; j++)      /*inner loop ton handle the unsorted part by comparing and swapping elements */
 		{
 			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
 				array[j] = array[j + 1];
-				array[j + 1] = temp;
-				flag = 1;
+				array[j + 1] = temp;    /* largest one bubble to the end */
+				flag = 1;               /* indiates swap occurred */
 				print_array(array, size);
 			}
 		}
